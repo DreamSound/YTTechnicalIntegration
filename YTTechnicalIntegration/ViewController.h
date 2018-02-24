9427e3c4f6495773a37c1ce5e12d899cda960e15
yangyuetong <yangyuetong@silversecuritytech.com>
2018-02-24 11:32:50 +0800

    Initial Commit

---
+++ YTTechnicalIntegration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTTechnicalIntegration
+//
+//  Created by hiteam on 2018/2/24.
+//  Copyright © 2018年 hiteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YTTechnicalIntegration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YTTechnicalIntegration
+//
+//  Created by hiteam on 2018/2/24.
+//  Copyright © 2018年 hiteam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
